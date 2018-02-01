741c6d34f7f1e39e37a5b8b1ecd6ea329ce64864
张鹏 <ex-zhangpeng012@pingan.com.cn>
2018-02-01 20:57:16 +0800

    初始化项目

---
+++ iOS11iPhoneXAdaptor/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOS11iPhoneXAdaptor
+//
+//  Created by 张鹏 on 2018/2/1.
+//  Copyright © 2018年 pingan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
